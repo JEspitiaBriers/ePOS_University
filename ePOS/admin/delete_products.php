6f64db2e96aa22680d7bf0f43b7d839e4874ac51
Dylan Jones <60651947+dylanjones784@users.noreply.github.com>
2022-04-19 18:38:09 +0100

    More Admin code

--- ePOS/admin/delete_products.php
+++ ePOS/admin/delete_products.php
@@ -1,3 +1,43 @@
 <?php
+require 'header.php';
+//this is a utility page with no html for the user to see
+
+if (isset($_POST['post']))
+{
+    require_once 'credentials.php';
+
+    $productID = $_POST['post'];
+
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+    if (!$connection)
+    {
+        die("Connection failed: " . $mysqli_connect_error);
+    }
+
+    $query = "DELETE FROM products WHERE productID = '$productID'";
+    $result = mysqli_query($connection,$query);
+
+    
+    //test so that if it actually happened, nothing else done here
+    if ($result)
+    {
+        echo "Product deleted";
+        // navigate back to the admin page:
+        header('Location: admin_products.php');
+    }
+    else
+    {    
+        $message = "Delete failed, please try again<br>";
+    }
+    
+    echo $message;
+    mysqli_close($connection);
+
+}
+else
+{
+    echo "No data was received. Go back to try again.";
+}
 
 ?>
--- ePOS/admin/edit_products.php
+++ ePOS/admin/edit_products.php
@@ -1,6 +1,57 @@
 <?php
+require_once 'credentials.php';
 
 // -- will just run a function to grab whatever product the admin just
 // --edited and update the db (once passing validation )
+// -- as of 19/04 no testing has been done on whether this works with the manage products table.
+$productID = "";
+$productName= "";
+$productImg = "";
+$productPrice = "";
+$productDesc = "";
+$noSoldProduct = "";
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if($_SESSION['username'] == "admin"){
+    if (isset($_POST['post'])){
+
+            
+        // Attempt to connect. Return an error if not.
+        if (!$connection)
+        {
+            die("Connection failed: " . $mysqli_connect_error);
+        }
+        $productID= $_POST['productID'];
+        $productName = $_POST['productName'];
+        $productImg = $_POST['productImage'];
+        $productPrice = $_POST['productPrice'];
+        $productDesc = $_POST['productDesc'];
+        $noSoldProduct = $_POST['noSold'];
+
+    
+    
+    
+    
+        $query = "UPDATE posts SET title = '$title', created = '$created',content = '$content' WHERE postid = '$pid'";
+
+        $result = mysqli_query($connection,$query);
+    
+        if ($result)
+        {
+            echo "Post edited.";
+            header('Location: manage_posts.php');
+        }
+        else
+        {    
+            // show an unsuccessful signup message:
+            $message = "Update failed, please try again<br>";
+        }
+        mysqli_close($connection);
+    
+    }else{
+        echo "No user data was received. Go back to try again.";
+        header('Location: manage_posts.php');
+    }
+
+}
 
 ?>
